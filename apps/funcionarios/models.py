057001e12885637e5e5ce13f9404ee2a8bdcd105
AlexPires1 <alexpirespereira747@gmail.com>
2020-02-24 17:16:50 -0300

    CRUD Documentos feito

--- apps/funcionarios/models.py
+++ apps/funcionarios/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from apps.departamentos.models import Departamentos
 from apps.empresas.models import Empresa
+from django.urls import reverse
 
 
 class Funcionario(models.Model):
@@ -10,6 +11,9 @@ class Funcionario(models.Model):
     departamentos = models.ManyToManyField(Departamentos)
     empresa = models.ForeignKey(
         Empresa, on_delete=models.PROTECT, null=True, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('list_funcionarios')
 
 
     def __str__(self):	    
--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'apps.documentos',
     'apps.registro_hora_extra',
     'apps.core',
+    'bootstrapform',
 ]
 
 MIDDLEWARE = [
@@ -89,8 +90,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "staticfiles"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 LOGIN_REDIRECT_URL = 'home'
 
